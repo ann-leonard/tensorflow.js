88d6e0e5e0f303534ecabca970bc48f472e52e04
sproulel13 <lucas@moven.com>
2019-02-26 22:45:01 -0500

    index html and setup for video

---
+++ tensorflow_intro/this.js
@@ -0,0 +1,14 @@
+//tf =require('@tensorflow/tfjs-node');
+//console.log(tf);
+// Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}

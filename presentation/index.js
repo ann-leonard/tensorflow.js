042f8df8b115e6b8d864d61e7916420cb6d5a579
ann-leonard <aleo0779@gmail.com>
2019-04-11 15:08:26 -0400

    error

--- presentation/index.js
+++ presentation/index.js
@@ -6,3 +6,9 @@ var tf =  require("@tensorflow/tfjs");
 
 var data = tf.data.csv('../tensorflow_intro/mnist/mnist_test.csv')
 console.log(data)
+
+async function init(){
+    var model = await tf.loadLayersModel('../tensorflow_intro/tfjs_model/model.json')
+    console.log("loaded")
+}
+init()

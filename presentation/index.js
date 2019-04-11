cc9427216c9bfb3945dd548d6bf70a5e28176f81
sproulel13 <lucas@moven.com>
2019-04-11 14:32:51 -0400

    tf-js

--- presentation/index.js
+++ presentation/index.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var NDarray = require('ndarray-imshow');
-var tf = require('@tensorflow/tfjs')
+var converter = require('@tensorflow/tfjs-converter');
+var tfn =  require("@tensorflow/tfjs-node");
+var tf =  require("@tensorflow/tfjs");
 
-var model=tf.loadModel('../tensorflow_intro/tfjs_model/model.json')
 var data = tf.data.csv('../tensorflow_intro/mnist/mnist_test.csv')
+console.log(data)
